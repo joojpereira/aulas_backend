863ee2483139e925b4eafa70eabf870d43b9c415
Joao Vitor Pereira Freitas <joao.268223@alunosatc.edu.br>
2025-08-07 20:19:40 -0300

    aula2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        String nome1 = "João";
+        int idade2 = 19;
+        double nota = 9.99;
+
+
+        System.out.printf("Nome: %s, Idade: %d\n", nome1, idade2);
+        System.out.printf("Nota final: %.2f\n", nota);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Nome: " + nome + "\n Idade: " + idade);
+
+        scanner.close();
     }
+
 }
